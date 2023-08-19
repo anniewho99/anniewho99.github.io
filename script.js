53527ece89f09cc48ccb18f117c653c2a5a5b3d3
xinyuehu-18 <xinyuehu@vassar.edu>
2023-08-19 13:01:22 -0700

    subgrid

--- script.js
+++ script.js
@@ -18,34 +18,52 @@ const GRIDS = [
 ];  
 
 function createSubGrids() {
-    const graphics = this.add.graphics({ lineStyle: { width: 2, color: 0x8B4513} });
+    const graphics = this.add.graphics({ lineStyle: { width: 2, color: 0x8B4513 } }); // Dark brown color
+    
     for (let grid of GRIDS) {
-        for (let y = grid.start[1]; y <= grid.end[1]; y++) {
-            for (let x = grid.start[0]; x <= grid.end[0]; x++) {
-                if (y === grid.start[1] || y === grid.end[1] || x === grid.start[0] || x === grid.end[0]) {
-                    graphics.strokeRect(x * cellSize, y * cellSize, cellSize, cellSize);
-                }
-            }
-        }
+        const startX = grid.start[0] * cellSize;
+        const startY = grid.start[1] * cellSize;
+        const endX = grid.end[0] * cellSize;
+        const endY = grid.end[1] * cellSize;
+
+        // Draw top horizontal wall
+        graphics.moveTo(startX, startY);
+        graphics.lineTo(endX, startY);
+
+        // Draw bottom horizontal wall
+        graphics.moveTo(startX, endY);
+        graphics.lineTo(endX, endY);
+
+        // Draw left vertical wall
+        graphics.moveTo(startX, startY);
+        graphics.lineTo(startX, endY);
+
+        // Draw right vertical wall
+        graphics.moveTo(endX, startY);
+        graphics.lineTo(endX, endY);
     }
     graphics.strokePath();
 }
 
+
 function isWall(x, y) {
     for (let grid of GRIDS) {
-        if (x >= grid.start[0] && x <= grid.end[0] && y >= grid.start[1] && y <= grid.end[1]) {
-            if ((x === grid.start[0] || x === grid.end[0] || y === grid.start[1] || y === grid.end[1])) {
-                // Check if the position is a door. If x, y is a door location, return false.
-                if ((x === grid.start[0] + 1 && y === grid.start[1]) || 
-                    (x === grid.start[0] + 1 && y === grid.end[1])) {
-                    return false; // This would make two doors on the top and bottom middle of the subgrid.
-                }
-                return true; // This position is a wall.
+        // If it's on the perimeter
+        if ((x === grid.start[0] || x === grid.end[0] || y === grid.start[1] || y === grid.end[1]) &&
+            x >= grid.start[0] && x <= grid.end[0] && y >= grid.start[1] && y <= grid.end[1]) {
+
+            // Check if the position is a door. If so, it's not a wall.
+            if ((x === grid.start[0] + 1 && y === grid.start[1]) || 
+                (x === grid.start[0] + 1 && y === grid.end[1])) {
+                return false;
             }
+
+            return true; // Otherwise, it's a wall.
         }
     }
     return false; // Default is not a wall.
 }
+
 
 let game = new Phaser.Game(config);
 
@@ -58,64 +76,47 @@ function preload() {
     this.load.image('player2', 'p2.png');
 }
 
-function create() {
-    // Create grid graphics
-    let graphics = this.add.graphics({ lineStyle: { width: 2, color: 0xFFFFFF } });
-    for (let i = 0; i <= gridSize; i++) {
-        graphics.moveTo(i * cellSize, 0);
-        graphics.lineTo(i * cellSize, gridSize * cellSize);
-        graphics.moveTo(0, i * cellSize);
-        graphics.lineTo(gridSize * cellSize, i * cellSize);
+function handleMovement(player, dx, dy) {
+    let potentialX = player.x + dx;
+    let potentialY = player.y + dy;
+
+    if (!isWall(potentialX / cellSize, potentialY / cellSize)) {
+        player.x = Phaser.Math.Clamp(potentialX, cellSize / 2, game.config.width - cellSize / 2);
+        player.y = Phaser.Math.Clamp(potentialY, cellSize / 2, game.config.height - cellSize / 2);
     }
-    graphics.strokePath();
-
-    createSubGrids.call(this);
-
-    player1 = this.add.sprite(cellSize / 2, cellSize / 2, 'player1').setScale(0.05); // Assuming your original image is twice the size of the cell.
-    player2 = this.add.sprite(this.sys.game.config.width - cellSize / 2, this.sys.game.config.height - cellSize / 2, 'player2').setScale(0.05);
-
-    // Keyboard controls
-    this.input.keyboard.on('keydown', handleKeyDown, this);
-}
-
-function update() {
-    // Any continual game logic goes here
 }
 
 function handleKeyDown(event) {
     switch (event.code) {
         // Player 1
         case 'ArrowUp':
-            player1.y = Phaser.Math.Clamp(player1.y - cellSize, cellSize / 2, game.config.height - cellSize / 2);
+            handleMovement(player1, 0, -cellSize);
             break;
         case 'ArrowDown':
-            player1.y = Phaser.Math.Clamp(player1.y + cellSize, cellSize / 2, game.config.height - cellSize / 2);
+            handleMovement(player1, 0, cellSize);
             break;
         case 'ArrowLeft':
-            player1.x = Phaser.Math.Clamp(player1.x - cellSize, cellSize / 2, game.config.width - cellSize / 2);
+            handleMovement(player1, -cellSize, 0);
             break;
         case 'ArrowRight':
-            player1.x = Phaser.Math.Clamp(player1.x + cellSize, cellSize / 2, game.config.width - cellSize / 2);
+            handleMovement(player1, cellSize, 0);
             break;
 
         // Player 2
         case 'KeyW':
-            player2.y = Phaser.Math.Clamp(player2.y - cellSize, cellSize / 2, game.config.height - cellSize / 2);
+            handleMovement(player2, 0, -cellSize);
             break;
         case 'KeyS':
-            player2.y = Phaser.Math.Clamp(player2.y + cellSize, cellSize / 2, game.config.height - cellSize / 2);
+            handleMovement(player2, 0, cellSize);
             break;
         case 'KeyA':
-            player2.x = Phaser.Math.Clamp(player2.x - cellSize, cellSize / 2, game.config.width - cellSize / 2);
+            handleMovement(player2, -cellSize, 0);
             break;
         case 'KeyD':
-            player2.x = Phaser.Math.Clamp(player2.x + cellSize, cellSize / 2, game.config.width - cellSize / 2);
+            handleMovement(player2, cellSize, 0);
             break;
     }
-
-    if (!isWall(potentialX / cellSize, potentialY / cellSize)) {
-        player1.x = potentialX;
-        player1.y = potentialY;
-    }
-
 }
+
+
+
