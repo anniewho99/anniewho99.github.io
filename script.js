fef343afe530993f0bf591c81e979827b173e0b1
xinyuehu-18 <xinyuehu@vassar.edu>
2023-09-05 18:08:59 -0700

    math round

--- script.js
+++ script.js
@@ -566,23 +566,23 @@ function onTokenHit(player, token) {
 
 function updatePlayerPosition(player1, player2) {
     // Calculate cellX and cellY for both players
-    const player1CellX = Math.floor(player1.x / cellWidth);
-    const player1CellY = Math.floor(player1.y / cellHeight);
-    const player2CellX = Math.floor(player2.x / cellWidth);
-    const player2CellY = Math.floor(player2.y / cellHeight);
+    const player1CellX = Math.round(player1.x / cellWidth);
+    const player1CellY = Math.round(player1.y / cellHeight);
+    const player2CellX = Math.round(player2.x / cellWidth);
+    const player2CellY = Math.round(player2.y / cellHeight);
     
     // Check if they are in the same cell
     if (player1CellX === player2CellX && player1CellY === player2CellY) {
-      // Apply an offset to each player's position
-      const offset = 7; // 5 pixels to the left and right
-      player1.x = cellToPixel(player1CellX) - offset;
-      player2.x = cellToPixel(player2CellX) + offset;
+        // Apply an offset to each player's position
+        const offset = 7; // 5 pixels to the left and right
+        player1.x = cellToPixel(player1CellX) - offset;
+        player2.x = cellToPixel(player2CellX) + offset;
     } else {
-      // Reset their positions so they are not offset when they are not in the same cell
-      player1.x = cellToPixel(player1CellX);
-      player2.x = cellToPixel(player2CellX);
+        // Reset their positions so they are not offset when they are not in the same cell
+        player1.x = cellToPixel(player1CellX);
+        player2.x = cellToPixel(player2CellX);
     }
-  }
+}
   
 function cellToPixel(cellX) {
     return cellX * cellWidth + cellWidth / 2;
