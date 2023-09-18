daa29355c87859930c3da2f227c830d52297d6c4
xinyuehu-18 <xinyuehu@vassar.edu>
2023-09-17 21:41:38 -0700

    scale

--- script.js
+++ script.js
@@ -536,12 +536,12 @@ function addStarTokens(scene, playerID) {
 
                 if (playerID === 0){
                     let star = scene.physics.add.sprite((x * cellWidth) - 30, (y * cellHeight) - 20, 'flower').setTint(color).setDepth(0);
-                    star.setScale(0.03);
+                    star.setScale(0.07);
                     star.color = color;  
                     scene.tokenGroup.add(star);
                 }else{
                     let star = scene.physics.add.sprite((x * cellWidth) - 30, (y * cellHeight) - 20, 'butterfly').setTint(color).setDepth(0);
-                    star.setScale(0.03);
+                    star.setScale(0.09);
                     star.color = color;  
                     scene.tokenGroup.add(star);
                 }
